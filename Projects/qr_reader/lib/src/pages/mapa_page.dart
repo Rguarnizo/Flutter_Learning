8e60e0cd31066a71c875c207462b69f1bd19d440
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2020-12-24 16:55:25 -0500

    Marked Map

--- Projects/qr_reader/lib/src/pages/mapa_page.dart
+++ Projects/qr_reader/lib/src/pages/mapa_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:qr_reader/src/providers/db_provider.dart';
 
+
 class MapaPage extends StatelessWidget {
   const MapaPage({Key key}) : super(key: key);
   
@@ -9,23 +10,25 @@ class MapaPage extends StatelessWidget {
   Widget build(BuildContext context) {
 
     final ScanModel scanModel = ModalRoute.of(context).settings.arguments;
+    final MapController mapController = MapController();
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Coordenadas QR'),
         actions: [
           IconButton(icon: Icon(Icons.my_location),onPressed: () {
-            
+            mapController.move(scanModel.getLatLng(),15);
           },)
         ],
       ),
-      body: createMap(scanModel),
+      body: createMap(scanModel,mapController),
     );
   }
 }
 
-createMap(ScanModel scan) {
+createMap(ScanModel scan,mapController) {
   return FlutterMap(    
+    mapController: mapController,
     key: UniqueKey(),
     options: MapOptions(      
       center: scan.getLatLng(),
@@ -33,8 +36,17 @@ createMap(ScanModel scan) {
     ),
     layers: [
       _crearMapa(),
+      _crearMarcadores(scan),
     ],
     );
+}
+
+_crearMarcadores(scan) {
+  return MarkerLayerOptions(
+    markers: <Marker>[
+      Marker(width: 100.0,height: 100.0,point: scan.getLatLng(),builder: (context) => Container(child: Icon(Icons.location_on,size:45,color: Theme.of(context).primaryColor,),)),
+    ]
+  );
 }
 
 _crearMapa() {
@@ -44,7 +56,8 @@ _crearMapa() {
     '{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}',
     additionalOptions: {      
       'accessToken': 'pk.eyJ1Ijoicmd1YXJuaXpvIiwiYSI6ImNrajNhcXo2cTJmNTcycW13YzV4c21qMGIifQ.-dHa_Cue38YShLF-vjMCGQ',      
-      'id': 'mapbox.mapbox-streets-v8'
+      'id': 'mapbox.satellite'
+      // No se admiten los estilos clasicos!
     }
   );
 }
