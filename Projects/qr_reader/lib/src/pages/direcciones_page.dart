facce72819a13e680c42c503078a4e406ab64165
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2020-12-24 17:12:31 -0500

    Direcciones Page

--- Projects/qr_reader/lib/src/pages/direcciones_page.dart
+++ Projects/qr_reader/lib/src/pages/direcciones_page.dart
@@ -1,13 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:qr_reader/src/bloc/scans_bloc.dart';
+import 'package:qr_reader/src/providers/db_provider.dart';
+import 'package:qr_reader/src/utils/utils.dart';
 
 
 class DireccionesPage extends StatelessWidget {
-  const DireccionesPage({Key key}) : super(key: key);
+  DireccionesPage({Key key}) : super(key: key);
+
+  final ScansBloc scansBloc = new ScansBloc();
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+
+    scansBloc.obtenerScans();
+
+    return StreamBuilder <List<ScanModel>>(
+      stream: scansBloc.scansStreamHttp,
+      builder: (context, snapshot) {
+        
+        if(!snapshot.hasData){
+          return Center(child: CircularProgressIndicator(),);
+        }
+
+        final scans = snapshot.data;
+
+        if(scans.length == 0){
+          return Center(
+            child: Text('No hay información para mostrar'),
+          );
+        }
+
+
+        return ListView.builder(
+          itemCount: scans.length,
+          itemBuilder: (context, index) => Dismissible(
+            key: UniqueKey(),
+            background: Container(color: Colors.red,),
+            onDismissed: (direction) {
+              scansBloc.borrarScan(scans[index].id);
+            },
+            child: ListTile(
+              leading: Icon(Icons.cloud_queue,color:Theme.of(context).primaryColor),
+              title: Text(scans[index].valor),
+              subtitle: Text('Id: ${scans[index].id}'),
+              trailing: Icon(Icons.keyboard_arrow_right_outlined),
+              onTap: (){
+                launchURL(scans[index],context);
+              },
+            ),
+          ),
+        );
+        },
+      );
   }
 }
