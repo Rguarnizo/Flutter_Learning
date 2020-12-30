e7ce31774456c8ba3a07f5a496a18a8cc1a1f4b9
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2020-12-30 18:25:00 -0500

    Form Validate

--- Projects/bloc_from_validation/lib/src/pages/producto_page.dart
+++ Projects/bloc_from_validation/lib/src/pages/producto_page.dart
@@ -1,7 +1,10 @@
+import 'package:bloc_from_validation/src/utils/utils.dart';
 import 'package:flutter/material.dart';
 
 class ProductoPage extends StatelessWidget {
-  const ProductoPage({Key key}) : super(key: key);
+  ProductoPage({Key key}) : super(key: key);
+
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -18,6 +21,7 @@ class ProductoPage extends StatelessWidget {
            child: Container(
              padding: EdgeInsets.all(15),
              child: Form(
+               key: _formKey,
                child: Column(
                  children: [
                    _crearNombre(),
@@ -36,6 +40,13 @@ class ProductoPage extends StatelessWidget {
     return TextFormField(
       textCapitalization: TextCapitalization.sentences,
       decoration: InputDecoration(labelText: 'producto'),
+      validator: (value){
+        if(value.length < 3){
+          return 'Ingrese el nombre del producto';
+        }else {
+          return null;
+        }
+      },
     );
   }
 
@@ -43,18 +54,28 @@ class ProductoPage extends StatelessWidget {
     return TextFormField(
       keyboardType: TextInputType.numberWithOptions(decimal:true),
       decoration: InputDecoration(labelText: 'precio',),
+      validator: (value){
+        if(isNumeric(value)){
+          return null;
+        }else{
+          return 'Ingrese un numero valido';
+        }
+      },
     );
   }
 
   _crearBoton() {
     return RaisedButton.icon(icon: Icon(Icons.save),onPressed: (){
-
+      _submit();
     },
     shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
     color: Colors.deepPurple,
     textColor: Colors.white,
     label: Text('Guardar'),
     );
+  }
 
+  void _submit(){
+    _formKey.currentState.validate();
   }
 }
