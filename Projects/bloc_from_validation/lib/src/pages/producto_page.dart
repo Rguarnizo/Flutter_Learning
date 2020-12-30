549d7b7d544b1f0a38d2228f4434259dd6d98260
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2020-12-30 17:59:10 -0500

    Product Page Desing

--- Projects/bloc_from_validation/lib/src/pages/producto_page.dart
+++ Projects/bloc_from_validation/lib/src/pages/producto_page.dart
@@ -10,8 +10,51 @@ class ProductoPage extends StatelessWidget {
         title: Text('Producto'),
         actions: [
           IconButton(icon: Icon(Icons.photo_size_select_actual), onPressed: (){}),
+          IconButton(icon: Icon(Icons.camera_alt), onPressed: (){}),
         ],
       ),
+      body: Container(
+         child: SingleChildScrollView(           
+           child: Container(
+             padding: EdgeInsets.all(15),
+             child: Form(
+               child: Column(
+                 children: [
+                   _crearNombre(),
+                   _crearPrecio(),
+                   _crearBoton(),
+                 ],
+               ),
+             ),
+           )
+         ),
+      ),
     );
+  }
+
+  Widget _crearNombre() {
+    return TextFormField(
+      textCapitalization: TextCapitalization.sentences,
+      decoration: InputDecoration(labelText: 'producto'),
+    );
+  }
+
+  _crearPrecio() {
+    return TextFormField(
+      keyboardType: TextInputType.numberWithOptions(decimal:true),
+      decoration: InputDecoration(labelText: 'precio',),
+    );
+  }
+
+  _crearBoton() {
+    return RaisedButton.icon(icon: Icon(Icons.save),onPressed: (){
+
+    },
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+    color: Colors.deepPurple,
+    textColor: Colors.white,
+    label: Text('Guardar'),
+    );
+
   }
 }
