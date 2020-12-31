753c137789a94a97901f1cdeca3e4dbf356057d6
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2020-12-30 19:20:40 -0500

    get Product of RealTime Database

--- Projects/bloc_from_validation/lib/src/pages/home_page.dart
+++ Projects/bloc_from_validation/lib/src/pages/home_page.dart
@@ -1,10 +1,11 @@
 import 'package:bloc_from_validation/src/bloc/provider.dart';
+import 'package:bloc_from_validation/src/providers/productos_provider.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key key}) : super(key: key);
+   HomePage({Key key}) : super(key: key);
 
-  
+  final productosProvider = new ProductosProvider();
 
   @override
   Widget build(BuildContext context) {
@@ -14,11 +15,24 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text('Home'),),
       floatingActionButton: _crearBoton(context),
-      body: SingleChildScrollView(child: Container()),
+      body: SingleChildScrollView(child: _crearListado()),
     );
   }
 
   _crearBoton(context) {
     return FloatingActionButton(onPressed: (){Navigator.pushNamed(context, 'producto');},child: Icon(Icons.add,color: Colors.deepPurple),);
+  }
+
+  _crearListado() {
+    return FutureBuilder(
+      future: productosProvider.cargarProductos(),
+      builder: (context, snapshot) {
+        if(snapshot.hasData){
+          return Container();
+        }else {
+          return Center(child: CircularProgressIndicator(),);
+        }
+      },
+    );
   }
 }
--- Projects/bloc_from_validation/lib/src/providers/productos_provider.dart
+++ Projects/bloc_from_validation/lib/src/providers/productos_provider.dart
@@ -22,4 +22,29 @@ class ProductosProvider{
     return true;
   }
 
+  Future<List<ProductoModel>> cargarProductos() async {
+    final url = '$_url/Productos.json';
+
+    final resp = await http.get(url);
+    
+    final Map<String,dynamic> decodedData = json.decode(resp.body);
+
+    final List<ProductoModel> productos = new List();
+
+
+    if(decodedData == null) return [];
+
+    decodedData.forEach((id, product) {
+      final prodTemp = ProductoModel.fromJson(product);
+      prodTemp.id = id;
+
+      productos.add(prodTemp);
+
+    });
+
+    print(productos);
+
+    return productos;
+  }
+
 }
