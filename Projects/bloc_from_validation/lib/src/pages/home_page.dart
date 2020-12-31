75aa32237b1d1e1fb9fbb8100a5986a92a1f1a34
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2020-12-31 11:59:07 -0500

    Show product home page.

--- Projects/bloc_from_validation/lib/src/pages/home_page.dart
+++ Projects/bloc_from_validation/lib/src/pages/home_page.dart
@@ -1,38 +1,68 @@
 import 'package:bloc_from_validation/src/bloc/provider.dart';
+import 'package:bloc_from_validation/src/models/producto_model.dart';
 import 'package:bloc_from_validation/src/providers/productos_provider.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-   HomePage({Key key}) : super(key: key);
+  HomePage({Key key}) : super(key: key);
 
   final productosProvider = new ProductosProvider();
 
   @override
   Widget build(BuildContext context) {
-    
     final bloc = Provider.of(context);
-    
+
     return Scaffold(
-      appBar: AppBar(title: Text('Home'),),
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
       floatingActionButton: _crearBoton(context),
-      body: SingleChildScrollView(child: _crearListado()),
+      body: Container(child: _crearListado()),
     );
   }
 
   _crearBoton(context) {
-    return FloatingActionButton(onPressed: (){Navigator.pushNamed(context, 'producto');},child: Icon(Icons.add,color: Colors.deepPurple),);
+    return FloatingActionButton(
+      onPressed: () {
+        Navigator.pushNamed(context, 'producto');
+      },
+      child: Icon(Icons.add, color: Colors.deepPurple),
+    );
   }
 
   _crearListado() {
     return FutureBuilder(
       future: productosProvider.cargarProductos(),
       builder: (context, snapshot) {
-        if(snapshot.hasData){
-          return Container();
-        }else {
-          return Center(child: CircularProgressIndicator(),);
+        if (snapshot.hasData) {
+          
+          final productos = snapshot.data;
+          print(productos);
+          return ListView.builder(
+            itemBuilder: (context, i) =>_crearItem(productos[i],context),
+            itemCount: productos.length,
+          );
+        } else {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
         }
       },
+    );
+  }
+
+  _crearItem(ProductoModel producto,context) {
+    return Dismissible(
+        key: UniqueKey(),
+        background: Container(color: Colors.red,),
+        onDismissed: (direccion){
+          //TODO: Borrar Productos.          
+        },
+        child: ListTile(
+        title: Text('${producto.titulo} - ${producto.valor}'),
+        subtitle: Text(producto.id),
+        onTap: () => Navigator.pushNamed(context,''),
+      ),
     );
   }
 }
--- Projects/bloc_from_validation/lib/src/providers/productos_provider.dart
+++ Projects/bloc_from_validation/lib/src/providers/productos_provider.dart
@@ -42,7 +42,7 @@ class ProductosProvider{
 
     });
 
-    print(productos);
+    
 
     return productos;
   }
