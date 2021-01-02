24cd3bda2658bfc4b33fb5cb81cd4163a67e1444
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-01-02 17:05:09 -0500

    HomePage Products Bloc

--- Projects/bloc_from_validation/lib/src/pages/home_page.dart
+++ Projects/bloc_from_validation/lib/src/pages/home_page.dart
@@ -1,23 +1,24 @@
 import 'package:bloc_from_validation/src/bloc/provider.dart';
 import 'package:bloc_from_validation/src/models/producto_model.dart';
-import 'package:bloc_from_validation/src/providers/productos_provider.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({Key key}) : super(key: key);
 
-  final productosProvider = new ProductosProvider();
+  
 
   @override
   Widget build(BuildContext context) {
-    final bloc = Provider.of(context);
+    
+    final productosBloc = Provider.productosBloc(context);
+    productosBloc.cargarProductos();
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
       ),
       floatingActionButton: _crearBoton(context),
-      body: Container(child: _crearListado()),
+      body: Container(child: _crearListado(productosBloc)),
     );
   }
 
@@ -30,16 +31,16 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  _crearListado() {
-    return FutureBuilder(
-      future: productosProvider.cargarProductos(),
+  _crearListado(ProductosBloc productosBloc) {
+    return StreamBuilder(
+      stream: productosBloc.productosStream,
       builder: (context, snapshot) {
         if (snapshot.hasData) {
           
           final productos = snapshot.data;
           print(productos);
           return ListView.builder(
-            itemBuilder: (context, i) =>_crearItem(productos[i],context),
+            itemBuilder: (context, i) =>_crearItem(productos[i],context,productosBloc),
             itemCount: productos.length,
           );
         } else {
@@ -51,13 +52,13 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  _crearItem(ProductoModel producto,context) {
+  _crearItem(ProductoModel producto,context,ProductosBloc productosBloc) {
     return Dismissible(
         key: UniqueKey(),
         background: Container(color: Colors.red,),
         onDismissed: (direccion){
           //TODO: Borrar Productos.        
-          productosProvider.borrarProducto(producto.id);  
+          productosBloc.borrarProducto(producto.id);
         },
         child: Card(
           child: Column(
