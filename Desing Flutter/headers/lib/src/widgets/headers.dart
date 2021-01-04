4466052f1e38b6900f120385bf2e3f9fb2770919
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-01-03 21:15:07 -0500

    Header diagonal

--- Desing Flutter/headers/lib/src/widgets/headers.dart
+++ Desing Flutter/headers/lib/src/widgets/headers.dart
@@ -18,9 +18,67 @@ class HeaderBordesRedondeados extends StatelessWidget {
     return Container(
       height: 300,
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.only(bottomLeft: Radius.circular(60),bottomRight: Radius.circular(60)),
+        borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(60), bottomRight: Radius.circular(60)),
         color: Color(0xff615AAB),
       ),
     );
   }
+}
+
+class HeaderDiagonal extends StatelessWidget {
+  const HeaderDiagonal({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+    borderRadius: BorderRadius.only(
+        bottomLeft: Radius.circular(60), bottomRight: Radius.circular(60)),
+        //color: Color(0xff615AAB),     
+        ),
+        child: CustomPaint(
+          painter: _HeaderDiagonalPainter(),
+        ),
+      );
+  }
+}
+
+
+class _HeaderDiagonalPainter extends CustomPainter{
+  @override
+  void paint(Canvas canvas, Size size) {
+      
+      //? Lapiz con el que dibujar
+      final paint = new Paint();
+
+      //?Propiedades
+      paint.color = Color(0xff615AAB);
+      paint.style = PaintingStyle.fill;
+      paint.strokeWidth = 5;
+
+      final path = new Path();
+
+
+      //?Dibujar con el path y el lapiz
+      path.lineTo(0, size.height*0.40);
+      path.lineTo(size.width,size.height*0.35);
+      path.lineTo(size.width,0);
+      path.lineTo(0,0);
+      
+
+      canvas.drawPath(path, paint);
+
+    }
+  
+    @override
+    bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    
+    return true;
+  }
+
+
+
 }
