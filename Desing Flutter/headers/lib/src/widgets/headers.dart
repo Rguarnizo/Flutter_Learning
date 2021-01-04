adea734c532c858dca697680c5c379c6b95f920b
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-01-03 21:26:19 -0500

    Header Picudo

--- Desing Flutter/headers/lib/src/widgets/headers.dart
+++ Desing Flutter/headers/lib/src/widgets/headers.dart
@@ -121,6 +121,62 @@ class _HeaderTriangularPainter extends CustomPainter{
 
       //?Dibujar con el path y el lapiz
       path.lineTo(size.width, size.height);
+      path.lineTo(0,size.height);
+      
+      
+
+      canvas.drawPath(path, paint);
+
+    }
+  
+    @override
+    bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    
+    return true;
+  }
+
+}
+
+class HeaderPicudo extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+    borderRadius: BorderRadius.only(
+        bottomLeft: Radius.circular(60), bottomRight: Radius.circular(60)),
+        //color: Color(0xff615AAB),     
+        ),
+        child: CustomPaint(
+          painter: _HeaderPicudoPainter(),
+        ),
+      );
+  }
+}
+
+
+class _HeaderPicudoPainter extends CustomPainter{
+  @override
+  void paint(Canvas canvas, Size size) {
+      
+      //? Lapiz con el que dibujar
+      final paint = new Paint();
+
+      //?Propiedades
+      paint.color = Color(0xff615AAB);
+      paint.style = PaintingStyle.fill;
+      paint.strokeWidth = 5;
+
+      final path = new Path();
+
+
+      //?Dibujar con el path y el lapiz
+      path.lineTo(0, size.height*0.3);
+      path.lineTo(size.width*0.5, size.height*0.35);
+      path.lineTo(size.width, size.height*0.3);
       path.lineTo(size.width,0);
       
       
