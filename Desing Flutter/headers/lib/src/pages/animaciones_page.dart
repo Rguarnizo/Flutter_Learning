5b24eb072e17e8f96eaecaf48bf373f0fffe5b0b
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-01-04 20:59:56 -0500

    Reto opacidad

--- Desing Flutter/headers/lib/src/pages/animaciones_page.dart
+++ Desing Flutter/headers/lib/src/pages/animaciones_page.dart
@@ -15,16 +15,14 @@ class AnimacionesPage extends StatelessWidget {
 }
 
 class _Rectangulo extends StatelessWidget {
-    @override
-    Widget build(BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
     return Container(
-       width: 70,
-       height: 70,
-       decoration: BoxDecoration(
-         color: Colors.blue
-       ),
-     );
-   }
+      width: 70,
+      height: 70,
+      decoration: BoxDecoration(color: Colors.blue),
+    );
+  }
 }
 
 class CuadradoAnimado extends StatefulWidget {
@@ -36,60 +34,76 @@ class CuadradoAnimado extends StatefulWidget {
   _CuadradoAnimadoState createState() => _CuadradoAnimadoState();
 }
 
-class _CuadradoAnimadoState extends State<CuadradoAnimado> with SingleTickerProviderStateMixin{
-
-  //! Es parecido a una barra de reproducción (Por ejemplo la de Youtube).  
+class _CuadradoAnimadoState extends State<CuadradoAnimado>
+    with SingleTickerProviderStateMixin {
+  //! Es parecido a una barra de reproducción (Por ejemplo la de Youtube).
   AnimationController _animationController;
-    
+
   Animation<double> rotacion;
   Animation<double> opacidad;
+  Animation<double> opacidadOut;
+  Animation<double> mover;
+  Animation<double> escalar;
 
   @override
-  void initState() { 
-    _animationController = new AnimationController(vsync: this,duration: Duration(milliseconds: 4000));
-    
-    rotacion = Tween(begin: 0.0,end: 2.0 * Math.pi).animate(
-      CurvedAnimation(parent: _animationController,curve: Curves.bounceOut)
-    );
+  void initState() {
+    _animationController = new AnimationController(
+        vsync: this, duration: Duration(milliseconds: 4000));
+
+    rotacion = Tween(begin: 0.0, end: 2.0 * Math.pi).animate(
+        CurvedAnimation(parent: _animationController, curve: Curves.bounceOut));
 
     //! Diferente Duración!
-    opacidad = Tween(begin: 0.1,end: 1.0).animate(CurvedAnimation(parent: _animationController,curve: Interval(0.5,1,curve: Curves.bounceOut)));
+    opacidad = Tween(begin: 0.1, end: 1.0).animate(CurvedAnimation(
+        parent: _animationController,
+        //! Interval me dice cuando empieza y cuando termina la animación (Porcentajes)
+        curve: Interval(0, 1, curve: Curves.bounceOut)));
 
+    opacidadOut = Tween(begin: 1.0,end: 0.0).animate(CurvedAnimation(parent: _animationController,curve: Interval(0.75,1,curve: Curves.easeInOut)));
+
+    
+    mover = Tween(begin: 0.0,end:200.0).animate(CurvedAnimation(parent: _animationController, curve: Curves.bounceOut));
+    escalar = Tween(begin: 0.0,end:2.0).animate(CurvedAnimation(parent: _animationController, curve: Curves.bounceOut));
 
     _animationController.addListener(() {
-      print('Status: ${_animationController.status}');
-      if(_animationController.status == AnimationStatus.completed){
+      
+      if (_animationController.status == AnimationStatus.completed) {
         _animationController.reverse();
-      }else if (_animationController.status == AnimationStatus.dismissed){
+      } else if (_animationController.status == AnimationStatus.dismissed) {
         _animationController.forward();
       }
     });
-    
+
     super.initState();
-    
   }
 
   @override
   void dispose() {
-
     _animationController.dispose();
     super.dispose();
-
   }
 
   @override
   Widget build(BuildContext context) {
-
     //? Play / Reproducción
     _animationController.forward();
 
-    return AnimatedBuilder(animation: _animationController,child: _Rectangulo() ,builder: (context, child) {
-      print('Rotación: ${rotacion.value}');
-      return Transform.rotate(angle: rotacion.value,child: Opacity(opacity: opacidad.value, child: child,));
-    },);
+    return AnimatedBuilder(
+      animation: _animationController,
+      child: _Rectangulo(),
+      builder: (context, child) {
+        print('Rotación: ${opacidad.status}');
+        print('Opacidad: ${rotacion.status}');
+        return Transform.translate(
+          offset: Offset(0,mover.value),
+          child: Transform.rotate(
+              angle: rotacion.value,
+              child: Opacity(
+                opacity: opacidadOut.value,
+                child: Transform.scale(scale: escalar.value,child: child),
+              )),
+        );
+      },
+    );
   }
 }
-
-
-
-
