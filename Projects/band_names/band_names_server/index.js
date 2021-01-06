73bd5640cda2dde476df499f46bb330d66a85c10
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-01-06 17:15:13 -0500

    Separate Server Files

--- Projects/band_names/band_names_server/index.js
+++ Projects/band_names/band_names_server/index.js
@@ -1,37 +1,21 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
-
-
-
-
 const app = express();
+
+//?Node Server.
+const server = require('http').createServer(app);
+module.exports.io = require('socket.io')(server);
+
+
+require('./sockets/socket');
+
+
+
 
 //?Path público
 const publicPath = path.resolve(__dirname,'public');
 app.use(express.static(publicPath));
-
-//?Node Server.
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-
-//? Mensajes de Sockets.
-io.on('connection',client =>{
-    console.log('Cliente conectado.')
-
-    client.on('disconnect', ()=>{
-        console.log('Cliente desconectado.')
-    });
-
-    client.on('mensaje',(mensaje)=>{
-        console.log(mensaje);
-        io.emit('mensaje',{admin:'Nuevo mensaje'});
-    })
-}
-)
-
-
-
 
 
 server.listen(process.env.PORT,(err)=>{
