84254bfb5aaae765d6cb7c5b7ddc1e98312b6796
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-01-06 17:04:22 -0500

    Emit and On Backend

--- Projects/band_names/band_names_server/index.js
+++ Projects/band_names/band_names_server/index.js
@@ -1,18 +1,41 @@
 const express = require('express');
 const path = require('path');
+require('dotenv').config();
+
+
+
 
 const app = express();
 
-//Path público
+//?Path público
 const publicPath = path.resolve(__dirname,'public');
-
 app.use(express.static(publicPath));
 
+//?Node Server.
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+//? Mensajes de Sockets.
+io.on('connection',client =>{
+    console.log('Cliente conectado.')
+
+    client.on('disconnect', ()=>{
+        console.log('Cliente desconectado.')
+    });
+
+    client.on('mensaje',(mensaje)=>{
+        console.log(mensaje);
+        io.emit('mensaje',{admin:'Nuevo mensaje'});
+    })
+}
+)
 
 
 
-app.listen(3000,(err)=>{
+
+
+server.listen(process.env.PORT,(err)=>{
     if(err) throw new Error(err);
 
-    console.log(`Sevidor corriendo en puerto ${3000}`);
+    console.log(`Sevidor corriendo en puerto ${process.env.PORT}`);
 })
