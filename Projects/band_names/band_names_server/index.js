e6cca3345a8f5722542af7509cfccb664597fddb
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-01-06 16:10:10 -0500

    Direcctorio publico

--- Projects/band_names/band_names_server/index.js
+++ Projects/band_names/band_names_server/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+//Path público
+const publicPath = path.resolve(__dirname,'public');
+
+app.use(express.static(publicPath));
+
 
 
 
 app.listen(3000,(err)=>{
     if(err) throw new Error(err);
 
-    console.log(`Sevidor corriendo en puerto ${3000}!!!!`);
+    console.log(`Sevidor corriendo en puerto ${3000}`);
 })
