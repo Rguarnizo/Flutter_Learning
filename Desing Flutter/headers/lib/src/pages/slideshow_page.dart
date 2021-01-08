b0cbb331c799b922bff9597441fa1aaf1c411604
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-01-07 21:30:39 -0500

    SlideShow Dots

--- Desing Flutter/headers/lib/src/pages/slideshow_page.dart
+++ Desing Flutter/headers/lib/src/pages/slideshow_page.dart
@@ -1,8 +1,6 @@
-
 import 'package:flutter/material.dart';
 
 import 'package:flutter_svg/flutter_svg.dart';
-
 
 class SlideShowPage extends StatelessWidget {
   const SlideShowPage({Key key}) : super(key: key);
@@ -10,12 +8,52 @@ class SlideShowPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: _Slides()),
-      
+      body: Center(
+          child: Column(
+        children: [
+          Expanded(child: _Slides()),
+          _Dots(),
+        ],
+      )),
     );
   }
 }
 
+class _Dots extends StatelessWidget {
+  const _Dots({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 70,
+      width: double.infinity,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          _Dot(),
+          _Dot(),
+          _Dot(),
+        ],
+      ),
+    );
+  }
+}
+
+class _Dot extends StatelessWidget {
+  const _Dot({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 12,
+      height: 12,
+      margin: EdgeInsets.symmetric(horizontal: 5),
+      decoration: BoxDecoration(color: Colors.grey, shape: BoxShape.circle),
+    );
+  }
+}
 
 class _Slides extends StatelessWidget {
   const _Slides({Key key}) : super(key: key);
@@ -26,8 +64,8 @@ class _Slides extends StatelessWidget {
       child: PageView(
         children: [
           _Slide(svg: 'assets/svg/slide-1.svg'),
-          _Slide(svg:'assets/svg/slide-2.svg'),
-          _Slide(svg:'assets/svg/slide-3.svg'),
+          _Slide(svg: 'assets/svg/slide-2.svg'),
+          _Slide(svg: 'assets/svg/slide-3.svg'),
         ],
       ),
     );
@@ -35,12 +73,8 @@ class _Slides extends StatelessWidget {
 }
 
 class _Slide extends StatelessWidget {
-
   final String svg;
-  const _Slide({
-    Key key,
-    this.svg
-  }) : super(key: key);
+  const _Slide({Key key, this.svg}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
