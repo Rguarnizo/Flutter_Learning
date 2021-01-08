cbb73bc5b98e6804e091342f7137c82495b80ffa
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-01-07 21:17:43 -0500

    Sliders and Slider

--- Desing Flutter/headers/lib/src/pages/slideshow_page.dart
+++ Desing Flutter/headers/lib/src/pages/slideshow_page.dart
@@ -10,8 +10,45 @@ class SlideShowPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: SvgPicture.asset('assets/svg/slide-1.svg'),),
+      body: Center(child: _Slides()),
       
+    );
+  }
+}
+
+
+class _Slides extends StatelessWidget {
+  const _Slides({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: PageView(
+        children: [
+          _Slide(svg: 'assets/svg/slide-1.svg'),
+          _Slide(svg:'assets/svg/slide-2.svg'),
+          _Slide(svg:'assets/svg/slide-3.svg'),
+        ],
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+
+  final String svg;
+  const _Slide({
+    Key key,
+    this.svg
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      padding: EdgeInsets.all(40),
+      child: SvgPicture.asset(svg),
     );
   }
 }
