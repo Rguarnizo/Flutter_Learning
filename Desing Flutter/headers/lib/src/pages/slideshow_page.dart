e4b8af68ef114f38e230160a472a6c9e754cef4b
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-01-07 21:10:32 -0500

    Use SVG Library

--- Desing Flutter/headers/lib/src/pages/slideshow_page.dart
+++ Desing Flutter/headers/lib/src/pages/slideshow_page.dart
@@ -1,6 +1,8 @@
 
 import 'package:flutter/material.dart';
 
+import 'package:flutter_svg/flutter_svg.dart';
+
 
 class SlideShowPage extends StatelessWidget {
   const SlideShowPage({Key key}) : super(key: key);
@@ -8,6 +10,7 @@ class SlideShowPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Center(child: SvgPicture.asset('assets/svg/slide-1.svg'),),
       
     );
   }
