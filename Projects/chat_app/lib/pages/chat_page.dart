abc9e86b46931fbf6ddd44cd2b61a8f0fa5d6558
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-01-10 15:55:38 -0500

    Chat Page advance

--- Projects/chat_app/lib/pages/chat_page.dart
+++ Projects/chat_app/lib/pages/chat_page.dart
@@ -6,9 +6,38 @@ class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hola Mundo'),
-     ),
-   );
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Row(children: [
+          CircleAvatar(child: Text('Te',style: TextStyle(fontSize: 15)),backgroundColor: Colors.blueAccent,),
+          SizedBox(width: 10,),
+          Text('Melissa Flores',style: TextStyle(color: Colors.black)),
+        ],),
+        
+      ),
+      body: Container(
+        child: Column(
+
+          children: [
+            Flexible(child: ListView.builder(
+              physics: BouncingScrollPhysics(),
+              itemBuilder: (_,i) => Text('$i'),
+              itemCount: 50,
+              reverse: true,
+            ),),
+
+
+            Divider(height: 1,),
+
+            Container(
+              color: Colors.white,
+              height: 50,
+            )
+
+
+          ],
+        ),
+      ),
+     );
   }
 }
