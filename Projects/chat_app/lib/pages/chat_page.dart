9b3cfe4a2f2a9d256e46b79c8870762d91fe6d1a
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-01-10 16:10:32 -0500

    TextField Mensaje

--- Projects/chat_app/lib/pages/chat_page.dart
+++ Projects/chat_app/lib/pages/chat_page.dart
@@ -1,43 +1,91 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+class ChatPage extends StatefulWidget {
+  @override
+  _ChatPageState createState() => _ChatPageState();
+}
 
-class ChatPage extends StatelessWidget {
+class _ChatPageState extends State<ChatPage> {
+  final textCtrl = new TextEditingController();
+  final focusNode = new FocusNode();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
-        title: Row(children: [
-          CircleAvatar(child: Text('Te',style: TextStyle(fontSize: 15)),backgroundColor: Colors.blueAccent,),
-          SizedBox(width: 10,),
-          Text('Melissa Flores',style: TextStyle(color: Colors.black)),
-        ],),
-        
-      ),
-      body: Container(
-        child: Column(
-
+        title: Row(
           children: [
-            Flexible(child: ListView.builder(
-              physics: BouncingScrollPhysics(),
-              itemBuilder: (_,i) => Text('$i'),
-              itemCount: 50,
-              reverse: true,
-            ),),
-
-
-            Divider(height: 1,),
-
-            Container(
-              color: Colors.white,
-              height: 50,
-            )
-
-
+            CircleAvatar(
+              child: Text('Te', style: TextStyle(fontSize: 15)),
+              backgroundColor: Colors.blueAccent,
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Text('Melissa Flores', style: TextStyle(color: Colors.black)),
           ],
         ),
       ),
-     );
+      body: Container(
+        child: Column(
+          children: [
+            Flexible(
+              child: ListView.builder(
+                physics: BouncingScrollPhysics(),
+                itemBuilder: (_, i) => Text('$i'),
+                itemCount: 50,
+                reverse: true,
+              ),
+            ),
+            Divider(
+              height: 1,
+            ),
+            Container(
+              color: Colors.white,
+              child: _inputChat(),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  _inputChat() {
+    return SafeArea(
+        child: Container(
+      margin: EdgeInsets.symmetric(horizontal: 8),
+      child: Row(
+        children: [
+          Flexible(
+            child: TextField(
+              controller: textCtrl,
+              onSubmitted: _handleSubmit,
+              onChanged: (String texto) {},
+              decoration: InputDecoration.collapsed(hintText: 'Enviar Mensaje'),
+              focusNode: focusNode,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 4.0),
+            child: Platform.isIOS
+                ? CupertinoButton(child: Text('Enviar'), onPressed: () {})
+                : Container(
+                    margin: EdgeInsets.symmetric(horizontal: 4.0),
+                    child:
+                        IconButton(icon: Icon(Icons.send), onPressed: () {},color: Colors.blue,)),
+          )
+        ],
+      ),
+    ));
+  }
+
+  _handleSubmit(String string){
+    print(string);
+    textCtrl.clear();
+    focusNode.requestFocus();
   }
 }
