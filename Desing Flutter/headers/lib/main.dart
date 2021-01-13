c6f65b6d6d9e6ae43f550a008ac1cb42ea70b547
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-01-12 19:53:45 -0500

    Init Project Pinterest

--- Desing Flutter/headers/lib/main.dart
+++ Desing Flutter/headers/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:headers/src/pages/animaciones_page.dart';
 import 'package:headers/src/labs/circular_progress_page.dart';
 import 'package:headers/src/pages/graficas_circulares_page.dart';
 import 'package:headers/src/pages/headers_page.dart';
+import 'package:headers/src/pages/pinterest_page.dart';
 import 'package:headers/src/pages/slideshow_page.dart';
  
 void main() => runApp(MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: SlideshowPage(),
+      home: PinterestPage(),
     );
   }
 } 
