06f2eb8c29dbf291ddc833c06b7a0250a7bf3a3f
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-01-13 19:57:40 -0500

    Buttom Desing

--- Desing Flutter/headers/lib/src/pages/emergency_page.dart
+++ Desing Flutter/headers/lib/src/pages/emergency_page.dart
@@ -7,10 +7,15 @@ class EmergencyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      
       body: Column(
         children: [
           PageHeader(),
-          BotonGordo(),
+          SingleChildScrollView(
+            child: Column(children: [
+              BotonGordo(),
+            ],)            
+          )
         ],
       ),
       
--- Desing Flutter/headers/lib/src/widgets/boton_gordo.dart
+++ Desing Flutter/headers/lib/src/widgets/boton_gordo.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BotonGordo extends StatelessWidget {
   const BotonGordo({Key key}) : super(key: key);
@@ -7,11 +7,13 @@ class BotonGordo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: _botonGordoBackground(),
+      child: Stack(children: [
+        _botonGordoBackground(),
+        _buttonDecoration(),
+      ]),
     );
   }
 }
-
 
 class _botonGordoBackground extends StatelessWidget {
   const _botonGordoBackground({Key key}) : super(key: key);
@@ -21,25 +23,70 @@ class _botonGordoBackground extends StatelessWidget {
     return Container(
       margin: EdgeInsets.all(20),
       decoration: BoxDecoration(
-        color: Colors.red,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.2),
-            offset: Offset(4,6),
-            blurRadius: 10
-          )
-        ],
-        borderRadius: BorderRadius.circular(15),
-        gradient: LinearGradient(
-          colors: [
+          color: Colors.red,
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black.withOpacity(0.2),
+                offset: Offset(4, 6),
+                blurRadius: 10)
+          ],
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(colors: [
             Color(0xff6989F5),
             Color(0xff906EF5),
-          ]
-        )
-      ),
+          ])),
       width: double.infinity,
       height: 100,
-      
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(15),
+          child: Stack(children: [
+          Positioned(
+            right: -20,
+            top: -20,
+              child: FaIcon(
+            FontAwesomeIcons.carCrash,
+            color: Colors.white.withOpacity(0.2),
+            size: 150,
+          )),
+        ]),
+      ),
+    );
+  }
+}
+
+class _buttonDecoration extends StatelessWidget {
+  const _buttonDecoration({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        SizedBox(
+          height: 140,
+          width: 30,
+        ),
+        Container(
+            child: FaIcon(
+          FontAwesomeIcons.carCrash,
+          color: Colors.white,
+          size: 40,
+        )),
+        SizedBox(
+          width: 20,
+        ),
+        Expanded(
+            child: Text(
+            'Motor Accident',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        Icon(
+          Icons.arrow_forward_ios,
+          color: Colors.white,
+        ),
+        SizedBox(width: 30),
+      ],
     );
   }
 }
