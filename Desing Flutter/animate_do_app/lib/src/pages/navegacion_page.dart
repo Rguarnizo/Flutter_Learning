1737d8c8be7b1dc0842d8a752b1d73037855c18c
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-01-16 18:09:18 -0500

    Notifications number with Provider

--- Desing Flutter/animate_do_app/lib/src/pages/navegacion_page.dart
+++ Desing Flutter/animate_do_app/lib/src/pages/navegacion_page.dart
@@ -1,16 +1,21 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:provider/provider.dart';
 
 class NavegacionPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Notification page'),
-        backgroundColor: Colors.pink,
+    return ChangeNotifierProvider(
+      create: (_) => _NotificationModel(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Notification page'),
+          backgroundColor: Colors.pink,
+        ),
+        floatingActionButton: BotonFlotante(),
+        bottomNavigationBar: BottomNavigation(),
       ),
-      floatingActionButton: BotonFlotante(),
-      bottomNavigationBar: BottomNavigation(),
     );
   }
 }
@@ -20,6 +25,9 @@ class BottomNavigation extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final int numero = Provider.of<_NotificationModel>(context).numero;
+
     return BottomNavigationBar(
       currentIndex: 0,
       selectedItemColor: Colors.pink,
@@ -32,13 +40,21 @@ class BottomNavigation extends StatelessWidget {
                 FaIcon(FontAwesomeIcons.bell),
                 //Positioned(child: Icon(Icons.brightness_1,size:8,color:Colors.pink),right: 0,top: 0,)
                 Positioned(
-                  child: Container(
-                    width: 12,
-                    height: 12,
-                    child: Text('99',style: TextStyle(color: Colors.white,fontSize: 7,fontWeight: FontWeight.bold)),
-                    decoration: BoxDecoration(
-                        color: Colors.pink, shape: BoxShape.circle),
-                        alignment: Alignment.center,
+                  child: BounceInDown(
+                    from: 20,
+                    animate: (numero >0)? true:false,
+                    child: Bounce(
+                      from:20,
+                      controller: (controller) =>Provider.of<_NotificationModel>(context).bounceController = controller,
+                      child: Container(
+                        width: 12,
+                        height: 12,
+                        child: Text('$numero',style: TextStyle(color: Colors.white,fontSize: 7,fontWeight: FontWeight.bold)),
+                        decoration: BoxDecoration(
+                            color: Colors.pink, shape: BoxShape.circle),
+                            alignment: Alignment.center,
+                      ),
+                    ),
                   ),
                   top: 0,
                   right: 0,
@@ -58,9 +74,33 @@ class BotonFlotante extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    
     return FloatingActionButton(
-      onPressed: () {},
+      onPressed: () {
+        Provider.of<_NotificationModel>(context,listen: false).numero++;    
+        Provider.of<_NotificationModel>(context,listen:false).bounceController.forward(from: 0.0);
+      },
       child: FaIcon(FontAwesomeIcons.play, color: Colors.pink),
     );
+  }
+}
+
+
+class _NotificationModel extends ChangeNotifier{
+
+  int _numero = 0;
+  AnimationController _bounceController;
+
+
+  int get numero => this._numero;
+
+  set numero(int numero){
+    this._numero = numero;
+    notifyListeners();
+  }
+  AnimationController get bounceController => this._bounceController;
+
+  set bounceController(AnimationController ctrl){
+    _bounceController = ctrl;
   }
 }
--- Desing Flutter/animate_do_app/lib/src/pages/pagina1_page.dart
+++ Desing Flutter/animate_do_app/lib/src/pages/pagina1_page.dart
@@ -3,35 +3,54 @@ import 'package:animate_do_app/src/pages/twitter_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:provider/provider.dart';
 
 
 class Pagina1Page extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Animate do'),
-        actions: [
-          IconButton(icon: FaIcon(FontAwesomeIcons.twitter), onPressed: ()=> Navigator.push(context,MaterialPageRoute(builder: (context) => TwitterPage()))),
-          IconButton(icon: Icon(Icons.navigate_next),onPressed: (){},)
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: ()=> Navigator.of(context).push(CupertinoPageRoute(builder: (_)=> Pagina1Page())),
-        child: FaIcon(FontAwesomeIcons.play),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-          ElasticIn(delay: Duration(milliseconds: 1100),child: Icon(Icons.new_releases)),
-          FadeInDown(delay: Duration(milliseconds:200),child: Text('Titulo',style: TextStyle(fontSize: 40,fontWeight: FontWeight.w200))),
-          FadeInDown(delay: Duration(milliseconds: 800),child: Text('Estoy Pequeño uwu',style: TextStyle(fontSize: 20,fontWeight: FontWeight.w200))),
-          FadeInLeft(delay: Duration(milliseconds: 1100),child: Container(color: Colors.blue,width: 200,height: 4,))
-        ],
-      ),
-     ),
-   );
+    return ChangeNotifierProvider(
+    create: (_) => _NotificationModel(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Animate do'),
+          actions: [
+            IconButton(icon: FaIcon(FontAwesomeIcons.twitter), onPressed: ()=> Navigator.push(context,MaterialPageRoute(builder: (context) => TwitterPage()))),
+            IconButton(icon: Icon(Icons.navigate_next),onPressed: (){},)
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: ()=> Navigator.of(context).push(CupertinoPageRoute(builder: (_)=> Pagina1Page())),
+          child: FaIcon(FontAwesomeIcons.play),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+            ElasticIn(delay: Duration(milliseconds: 1100),child: Icon(Icons.new_releases)),
+            FadeInDown(delay: Duration(milliseconds:200),child: Text('Titulo',style: TextStyle(fontSize: 40,fontWeight: FontWeight.w200))),
+            FadeInDown(delay: Duration(milliseconds: 800),child: Text('Estoy Pequeño uwu',style: TextStyle(fontSize: 20,fontWeight: FontWeight.w200))),
+            FadeInLeft(delay: Duration(milliseconds: 1100),child: Container(color: Colors.blue,width: 200,height: 4,))
+          ],
+        ),
+       ),
+   ),
+    );
+  }
+}
+
+
+
+class _NotificationModel extends ChangeNotifier{
+
+  int _numero = 0;
+
+
+  int get numero => this._numero;
+
+  set numero(int numero){
+    this._numero = numero;
+    notifyListeners();
   }
 }
