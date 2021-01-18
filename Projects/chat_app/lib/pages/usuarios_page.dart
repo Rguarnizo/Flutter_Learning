de6bae76d1f5821d055cbe2b0592e1dc61b365a0
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-01-17 19:43:55 -0500

    LogOut

--- Projects/chat_app/lib/pages/usuarios_page.dart
+++ Projects/chat_app/lib/pages/usuarios_page.dart
@@ -1,5 +1,7 @@
 import 'package:chat_app/models/usaurio.dart';
+import 'package:chat_app/services/auth_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class UsuariosPage extends StatefulWidget {
@@ -22,20 +24,29 @@ class _UsuariosPageState extends State<UsuariosPage> {
 
   @override
   Widget build(BuildContext context) {
+
+    final authService = Provider.of<AuthService>(context);
+    final usuario = authService.usuario;
+
     return Scaffold(
         appBar: AppBar(
           title: Text(
-            'Mi nombre',
+            usuario.nombre,
             style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
           ),
           elevation: 1,
           backgroundColor: Colors.white,
           leading: IconButton(
+            
             icon: Icon(
               Icons.exit_to_app,
               color: Colors.black,
             ),
-            onPressed: () {},
+            onPressed: () {
+              //TODO: Desconectarnos del socket server.              
+              Navigator.pushReplacementNamed(context, 'login');
+              AuthService.delateToken();
+            },
           ),
           actions: [
             Container(
