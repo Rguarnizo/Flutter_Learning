cbfb0a9e490202162b529aea634ac0132d0c50c4
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-01-18 20:13:51 -0500

    Send Token through extra headers

--- Projects/chat_app/lib/services/socket_service.dart
+++ Projects/chat_app/lib/services/socket_service.dart
@@ -1,38 +1,41 @@
 import 'package:chat_app/global/environment.dart';
+import 'package:chat_app/services/auth_service.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 
-enum ServerStatus{
+enum ServerStatus {
   Online,
   Offline,
   Connecting,
 }
 
-class SocketService with ChangeNotifier{
-
+class SocketService with ChangeNotifier {
   ServerStatus _serverStatus = ServerStatus.Connecting;
   IO.Socket _socket;
   Function get emit => this._socket.emit;
 
-  
-
   IO.Socket get socket => _socket;
   ServerStatus get serverStatus => this._serverStatus;
 
-  void connect(){
-      _socket = IO.io(Enviroments.socketUrl,
-      IO.OptionBuilder().setTransports(['websocket']).enableForceNew()
-      .build());
-      
+  void connect() async {
+    final token = await AuthService.getToken();
 
-      _socket.onConnect((_){
+    _socket = IO.io(
+        Enviroments.socketUrl,
+        IO.OptionBuilder()
+            .setTransports(['websocket'])
+            .enableForceNew()
+            .setExtraHeaders({'x-token': token})
+            .build());
+
+    _socket.onConnect((_) {
       print('connect');
-      _socket.emit('msg','test');
+      _socket.emit('msg', 'test');
       this._serverStatus = ServerStatus.Online;
       notifyListeners();
-      }); 
+    });
 
-    _socket.on('event',(data) => print(data));
+    _socket.on('event', (data) => print(data));
 
     _socket.onDisconnect((_) {
       this._serverStatus = ServerStatus.Offline;
@@ -41,17 +44,12 @@ class SocketService with ChangeNotifier{
 
     _socket.on('fromServer', (_) => print(_));
 
-    _socket.on('mensaje',(payload){
+    _socket.on('mensaje', (payload) {
       print('Mensaje: ' + payload['admin']);
     });
   }
 
-  void disconnect(){
+  void disconnect() {
     this._socket.disconnect();
   }
-
-
-
-
-
 }
