40c627bf6ec6ef56e23f79cf4b2b807c019f46b9
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-01-20 22:54:32 -0500

    Service of List of messages

--- Projects/chat_app/lib/services/chat_service.dart
+++ Projects/chat_app/lib/services/chat_service.dart
@@ -1,15 +1,22 @@
+import 'package:chat_app/global/environment.dart';
+import 'package:chat_app/models/mensajes_response.dart';
 import 'package:chat_app/models/usaurio.dart';
+import 'package:chat_app/services/auth_service.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-class ChatService with ChangeNotifier{
-
-
+class ChatService with ChangeNotifier {
   Usuario usuarioPara;
 
-  Future getChat(String usuarioID)async{
-    
+  Future<List<Mensaje>> getChat(String usuarioID) async {
+    final resp = await http.get(Enviroments.apiUrl + '/mensajes/$usuarioID',
+        headers: {
+          'Content-Type': 'application/json',
+          'x-token': await AuthService.getToken()});
+
+
+    final mensajesResp = mensajesResponseFromJson(resp.body);
+
+    return mensajesResp.mensajes;
   }
-
-
-
 }
