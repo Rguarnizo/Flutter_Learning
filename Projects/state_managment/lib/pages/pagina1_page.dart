d5f8dcc05c79f8d44e291500de4d0c3908c11c9f
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-01-23 14:58:43 -0500

    Import Provider and fix little bug

--- Projects/state_managment/lib/pages/pagina1_page.dart
+++ Projects/state_managment/lib/pages/pagina1_page.dart
@@ -11,7 +11,7 @@ class Pagina1Page extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.accessibility_new),
-        onPressed: () => Navigator.pushNamed(context, 'pagina 2'),
+        onPressed: () => Navigator.pushNamed(context, 'pagina2'),
       ),
       
       body: Center(
