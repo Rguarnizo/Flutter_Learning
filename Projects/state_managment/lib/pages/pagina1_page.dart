137d7f9a5f116380a5b866ab6b678af00f419276
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-01-22 22:34:04 -0500

    Page 1 desing

--- Projects/state_managment/lib/pages/pagina1_page.dart
+++ Projects/state_managment/lib/pages/pagina1_page.dart
@@ -15,8 +15,38 @@ class Pagina1Page extends StatelessWidget {
       ),
       
       body: Center(
-        child: Text('Hola Mundo'),
+        child: InformacionUsuario(),
      ),
    );
+  }
+}
+
+
+class InformacionUsuario extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+
+        Text('General',style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+        Divider(),
+        ListTile(title: Text('Nombre: '),),
+        ListTile(title: Text('Edad: '),),
+        
+        Text('General',style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+        Divider(),
+
+        ListTile(title: Text('Profesion 1'),),
+        ListTile(title: Text('Profesion 1'),),
+        ListTile(title: Text('Profesion 1'),),
+
+      ],),
+    );
   }
 }
