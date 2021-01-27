7a8bd75b179e8386743bbd8780cd91d16671db6f
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-01-27 18:31:11 -0500

    LauncherPage Tablet and Phone

---
+++ Desing Flutter/Desings/lib/src/pages/launcher_tablet_page.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:headers/src/theme/theme.dart';
+import 'package:provider/provider.dart';
+import '../routes/routes.dart';
+
+class LauncherTabletPage extends StatelessWidget {
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+
+    final appTheme = Provider.of<ThemeChanger>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Diseños Flutter - Tablet'),
+        backgroundColor: appTheme.currentTheme.accentColor,
+      ),
+      drawer: _MenuPrincipal(),
+      body: _ListaOpciones(),
+    );
+  }
+}
+
+class _ListaOpciones extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    final appTheme = Provider.of<ThemeChanger>(context);
+
+
+    return ListView.separated(
+        physics: BouncingScrollPhysics(),
+        itemBuilder: (_, i) => ListTile(
+              leading: FaIcon(
+                pageRoutes[i].icon,
+                color: appTheme.currentTheme.accentColor
+              ),
+              title: Text(pageRoutes[i].titulo),
+              trailing: Icon(Icons.chevron_right),
+              onTap: () => Navigator.push(context, MaterialPageRoute(builder: (_)=> pageRoutes[i].page)),
+              
+            ),
+        separatorBuilder: (_, i) => Divider(
+              color: Colors.blue,
+            ),
+        itemCount: pageRoutes.length);
+  }
+}
+
+class _MenuPrincipal extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    
+    
+    final appTheme = Provider.of<ThemeChanger>(context);
+
+
+    return Drawer(
+      child: Container(
+        child: Column(
+          children: [
+            SafeArea(
+                child: Container(
+                padding: EdgeInsets.all(20),
+                width: double.infinity,
+                height: 200,
+                child: CircleAvatar(
+                  child: Text(
+                    'FH',
+                    style: TextStyle(fontSize: 50),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: _ListaOpciones(),
+            ),
+            ListTile(
+              leading: Icon(Icons.lightbulb_outline,color: appTheme.currentTheme.accentColor),
+              title:Text('Dark Mode'),
+              trailing: Switch.adaptive(value: appTheme.darkTheme, onChanged: (value){
+                appTheme.darkTheme = value;
+              }),
+            ),
+            ListTile(
+              leading: Icon(Icons.dashboard_customize,color: appTheme.currentTheme.accentColor),
+              title:Text('Custom Theme'),
+              trailing: Switch.adaptive(value: appTheme.customTheme, onChanged: (value){
+                appTheme.customTheme = value;
+              }),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
