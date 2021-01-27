21569b71018c7368983fd6c22f7458660fe32137
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-01-26 19:16:33 -0500

    Using BLoC builder to read state

--- Projects/state_managment/lib/pages/pagina1_page.dart
+++ Projects/state_managment/lib/pages/pagina1_page.dart
@@ -1,30 +1,33 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:state_managment/bloc/usuario/usuario_bloc.dart';
 
 class Pagina1Page extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Pagina1'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.accessibility_new),
-        onPressed: () => Navigator.pushNamed(context, 'pagina 2'),
-      ),
-      
-      body: Center(
-        child: InformacionUsuario(),
-     ),
-   );
+        appBar: AppBar(
+          title: Text('Pagina1'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.accessibility_new),
+          onPressed: () => Navigator.pushNamed(context, 'pagina2'),
+        ),
+        body: BlocBuilder<UsuarioBloc, UsuarioState>(
+          builder: (context, state) {
+            if (state.existeUsuario) {
+              return Center(
+                child: InformacionUsuario(),
+              );
+            }else{
+              return Center(child: Text('No hay un usuario seleccionado'),);
+            }
+          },
+        ));
   }
 }
 
-
 class InformacionUsuario extends StatelessWidget {
-  
-
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -33,20 +36,33 @@ class InformacionUsuario extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-
-        Text('General',style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
-        Divider(),
-        ListTile(title: Text('Nombre: '),),
-        ListTile(title: Text('Edad: '),),
-        
-        Text('General',style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
-        Divider(),
-
-        ListTile(title: Text('Profesion 1'),),
-        ListTile(title: Text('Profesion 1'),),
-        ListTile(title: Text('Profesion 1'),),
-
-      ],),
+          Text(
+            'General',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          Divider(),
+          ListTile(
+            title: Text('Nombre: '),
+          ),
+          ListTile(
+            title: Text('Edad: '),
+          ),
+          Text(
+            'General',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          Divider(),
+          ListTile(
+            title: Text('Profesion 1'),
+          ),
+          ListTile(
+            title: Text('Profesion 1'),
+          ),
+          ListTile(
+            title: Text('Profesion 1'),
+          ),
+        ],
+      ),
     );
   }
 }
