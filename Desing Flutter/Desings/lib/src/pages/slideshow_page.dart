bed9fc00deecf1d8646e52f0c57bd65de5266029
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-01-27 19:31:22 -0500

    Slidershow Row and Column

--- Desing Flutter/Desings/lib/src/pages/slideshow_page.dart
+++ Desing Flutter/Desings/lib/src/pages/slideshow_page.dart
@@ -11,45 +11,66 @@ class SlideshowPage extends StatelessWidget {
   Widget build(BuildContext context) {
 
     final themeData = Provider.of<ThemeChanger>(context);
+    bool isLarge;
+    if(MediaQuery.of(context).size.height>500){
+      isLarge = true;
+    }else{
+      isLarge = false;
+    }
 
     return Scaffold(
-      body: Column(
+      body:(isLarge)? Column(
         children: [
           Expanded(
                       child: Center(
         
-        child: SlideShow(slides: [
-            SvgPicture.asset('assets/svg/slide-1.svg'),
-            SvgPicture.asset('assets/svg/slide-2.svg'),
-            SvgPicture.asset('assets/svg/slide-3.svg'),
-            SvgPicture.asset('assets/svg/slide-4.svg'),
-            SvgPicture.asset('assets/svg/slide-5.svg'),
-            
-        ],
-        colorPrimario:themeData.currentTheme.accentColor,
-        
-        bulletPrimario: 15,
-        bulletSecundario: 12,),
+        child: MiSlideShow(themeData: themeData),
      ),
           ),Expanded(
             child: Center(
           
-          child: SlideShow(slides: [
-            SvgPicture.asset('assets/svg/slide-1.svg'),
-            SvgPicture.asset('assets/svg/slide-2.svg'),
-            SvgPicture.asset('assets/svg/slide-3.svg'),
-            SvgPicture.asset('assets/svg/slide-4.svg'),
-            SvgPicture.asset('assets/svg/slide-5.svg'),
-            
-          ],
-          colorPrimario: themeData.currentTheme.accentColor,
-          colorSecundario: themeData.currentTheme.buttonColor,
-          bulletPrimario: 15,
-          bulletSecundario: 12,),
+          child: MiSlideShow(themeData: themeData),
        ),
      ),
         ],
-      )
+      ):Row(children: [
+Expanded(
+                      child: Center(
+        
+        child: MiSlideShow(themeData: themeData),
+     ),
+          ),Expanded(
+            child: Center(
+          
+          child: MiSlideShow(themeData: themeData),
+       ),
+     ),
+      ],)
    );
+  }
+}
+
+class MiSlideShow extends StatelessWidget {
+  const MiSlideShow({
+    Key key,
+    @required this.themeData,
+  }) : super(key: key);
+
+  final ThemeChanger themeData;
+
+  @override
+  Widget build(BuildContext context) {
+    return SlideShow(slides: [
+        SvgPicture.asset('assets/svg/slide-1.svg'),
+        SvgPicture.asset('assets/svg/slide-2.svg'),
+        SvgPicture.asset('assets/svg/slide-3.svg'),
+        SvgPicture.asset('assets/svg/slide-4.svg'),
+        SvgPicture.asset('assets/svg/slide-5.svg'),
+        
+    ],
+    colorPrimario:themeData.currentTheme.accentColor,
+    
+    bulletPrimario: 15,
+    bulletSecundario: 12,);
   }
 }
