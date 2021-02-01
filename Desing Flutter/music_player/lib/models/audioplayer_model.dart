8a40db4897df15cedfd183c0a208b1ee180fed49
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-01-31 23:52:23 -0500

    Connecting button to animate disco

--- Desing Flutter/music_player/lib/models/audioplayer_model.dart
+++ Desing Flutter/music_player/lib/models/audioplayer_model.dart
@@ -6,6 +6,9 @@ class AudioPlayerModel with ChangeNotifier{
 
   bool _playing = false;
   AnimationController _controller;
+  Duration _songDuration = new Duration(milliseconds: 0);
+  Duration _songCurrentDuration = new Duration(milliseconds: 0);
+  
 
   set controller(AnimationController controller){
     this._controller = controller;
