fecbeae7c3af64853880f173009303f3fb9667d1
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-01-31 23:45:02 -0500

    Model to audioPlayer and Provider injection

--- Desing Flutter/music_player/lib/main.dart
+++ Desing Flutter/music_player/lib/main.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:music_player/models/audioplayer_model.dart';
 import 'package:music_player/pages/music_player.dart';
 import 'package:music_player/themes/theme.dart';
+import 'package:provider/provider.dart';
  
 void main() => runApp(MyApp());
  
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Music Player',
-      theme: miTema,
-      home: MusicPlayerPage(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AudioPlayerModel())
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Music Player',
+        theme: miTema,
+        home: MusicPlayerPage(),
+      ),
     );
   }
 }
