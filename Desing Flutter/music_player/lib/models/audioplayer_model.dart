3ba930938cc3fc82f01722c5efe176d40d8685b2
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-02-01 20:51:17 -0500

    Setter getter of playing

--- Desing Flutter/music_player/lib/models/audioplayer_model.dart
+++ Desing Flutter/music_player/lib/models/audioplayer_model.dart
@@ -21,11 +21,20 @@ class AudioPlayerModel with ChangeNotifier{
 
   set songDuration (Duration duration){
     this._songDuration;
+    notifyListeners();
   }
 
   
   set songCurrentDuration (Duration duration){
     this._songCurrentDuration;
+    notifyListeners();
+  }
+
+  bool get playing => this._playing;
+
+  set playing(bool playing){
+    this._playing = playing;
+    notifyListeners();
   }
 
 }
