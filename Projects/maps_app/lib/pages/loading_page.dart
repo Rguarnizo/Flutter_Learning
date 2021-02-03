548ae1ad72bed2766b7ad498401382ab1e30fe72
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-02-03 13:30:46 -0500

    Loading page handle permissions

--- Projects/maps_app/lib/pages/loading_page.dart
+++ Projects/maps_app/lib/pages/loading_page.dart
@@ -1,32 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:maps_app/helpers/helpers.dart';
 import 'package:maps_app/pages/gps_access.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 import 'map_page.dart';
 
-
 class LoadingPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: FutureBuilder(
-        future: this.checkGpsLocation(context),
-        builder: (context, snapshot) {
-          return Center(
-            child: CircularProgressIndicator(strokeWidth: 2,)
-     );
-        }
-      ),
-   );
+          future: this.checkGpsLocation(context),
+          builder: (context, snapshot) {
+            return Center(
+                child: CircularProgressIndicator(
+              strokeWidth: 2,
+            ));
+          }),
+    );
   }
-  
+
   Future checkGpsLocation(BuildContext context) async {
-     await Future.delayed(Duration(milliseconds: 100));
+    //*Permiso GPS
+    final gpsPermission = await Permission.location.isGranted;
+    //* GPS ACTIVO?
+    final gpsActive = await Geolocator.isLocationServiceEnabled();
 
-     //Navigator.pushReplacement(context,navegarMapFadeIn(context, MapPage()));
-     //Navigator.pushReplacement(context,navegarMapFadeIn(context, GpsAccessPage()));
-     print('Loading page Hola mundo!');
-   }
-
+    await Future.delayed(Duration(milliseconds: 100));
+    if (gpsActive && gpsPermission) {
+      Navigator.pushReplacement(context, navegarMapFadeIn(context, MapPage()));
+      return '';
+    } else if(!gpsActive){      
+      return 'Active el GPS';
+    }else if(!gpsPermission){
+      Navigator.pushReplacement(context,navegarMapFadeIn(context, GpsAccessPage()));      
+      return 'El permiso de GPS es necesario';
+    }
+  }
 }
