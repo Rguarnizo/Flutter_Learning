3646506cd99157694d679108c7a8336fd19286f1
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-02-03 23:54:12 -0500

    Show modal information, Status Permantly Denied

--- Projects/maps_app/lib/helpers/modal_popup.dart
+++ Projects/maps_app/lib/helpers/modal_popup.dart
@@ -1,8 +1,33 @@
 part of 'helpers.dart';
 
-
-
-
-showModal(BuildContext context,Widget widget){
-  showModalBottomSheet(context: context, builder: (context) => widget);
+showModal(BuildContext context,
+    {String title, String subtitle, Function onAccept, Function onDismiss}) {
+  showModalBottomSheet(
+      context: context,
+      builder: (context) => Container(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              children: [
+                Text(
+                  title,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                ),
+                Container(
+                  padding: EdgeInsets.all(10),
+                    child: Text(
+                  subtitle,
+                  style: TextStyle(fontSize: 18),
+                )),
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [                   
+                      MaterialButton(onPressed: onDismiss,color: Colors.red,child: Text('Despues...',style: TextStyle(color: Colors.white),),),
+                      MaterialButton(onPressed: onAccept,color: Colors.green,child: Text('¡Vamos a ello!',style: TextStyle(color: Colors.white)),),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ));
 }
--- Projects/maps_app/lib/pages/permissions_page.dart
+++ Projects/maps_app/lib/pages/permissions_page.dart
@@ -4,13 +4,9 @@ import 'package:maps_app/bloc/permission_bloc.dart';
 import 'package:maps_app/helpers/helpers.dart';
 import 'package:permission_handler/permission_handler.dart';
 
-
 class PermissionsPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-
-
     final permissionBloc = BlocProvider.of<PermissionBloc>(context);
 
     return Scaffold(
@@ -19,7 +15,7 @@ class PermissionsPage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text('To use this application you need active GPS'),
-            MaterialButton(              
+            MaterialButton(
               child: Text(
                 'Solicitar Acceso',
                 style: TextStyle(color: Colors.white),
@@ -28,18 +24,23 @@ class PermissionsPage extends StatelessWidget {
               shape: StadiumBorder(),
               elevation: 0,
               splashColor: Colors.transparent,
-              onPressed: () async{               
-               final status = await Permission.location.request();
-               print(status);
-               if(status == PermissionStatus.permanentlyDenied){
-                 showModal(context,PermissionsPage());
-               }
-               permissionBloc.add(PermissionCheck(permissionStatus: status));                                                                                               
+              onPressed: () async {
+                final status = await Permission.location.request();
+                print(status);
+                if (status == PermissionStatus.permanentlyDenied) {
+                  showModal(context,
+                      title: 'Ouch! Permanentemente denegado 😵',
+                      subtitle:
+                          'Al parecer tienen denegado el acceso al GPS, entra a la configuración de permisos y activala',
+                      onAccept: () => openAppSettings(),
+                      onDismiss: () => Navigator.pop(context));
+                }
+                permissionBloc.add(PermissionCheck(permissionStatus: status));
               },
             )
           ],
         ),
-     ),
-   );
+      ),
+    );
   }
 }
