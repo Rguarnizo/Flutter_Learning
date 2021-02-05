f096a56dfdbe12b9385945d51c8da2e1ef8dc1b4
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-02-05 18:00:03 -0500

    Create Button of activate/deactivate polylines

---
+++ Projects/maps_app/lib/widgets/my_route_button.dart
@@ -0,0 +1,22 @@
+part of 'widgets.dart';
+
+class MyRouteButton extends StatelessWidget {
+  final onPress;
+
+  const MyRouteButton({Key key, @required this.onPress}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 10),
+      child: CircleAvatar(
+        backgroundColor: Colors.white,
+        maxRadius: 25,
+        child: IconButton(
+          icon: Icon(Icons.my_location, color: Colors.black87),
+          onPressed: onPress,
+        ),
+      ),
+    );
+  }
+}
--- Projects/maps_app/lib/widgets/widgets.dart
+++ Projects/maps_app/lib/widgets/widgets.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+
 import 'package:meta/meta.dart';
 
 part 'my_location_button.dart';
+part 'my_route_button.dart';
