0b29d6bd9ebee9ab0773f9b01a88c48f16459d47
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-02-07 23:24:16 -0500

    Show/Hidden Search bar aggre Bloc State

--- Projects/maps_app/lib/widgets/search_bar.dart
+++ Projects/maps_app/lib/widgets/search_bar.dart
@@ -5,19 +5,28 @@ class SearchBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return BlocBuilder<SearchBloc,SearchState>(
+      builder: (context, state) {
+        return state.seleccionManual? Container():_buildSearchBar(context);
+      },
+    );
+  }
+
+  Widget _buildSearchBar(BuildContext context) {
     final size = MediaQuery.of(context).size;
 
     return SafeArea(
       child: Container(
         padding: EdgeInsets.symmetric(horizontal: 30),
-        width: size.width,        
+        width: size.width,
         child: GestureDetector(
-          onTap: ()async {
-            final result = await showSearch(context: context, delegate: SearchDestination());
-            returnSearch(context,result);
+          onTap: () async {
+            final result = await showSearch(
+                context: context, delegate: SearchDestination());
+            returnSearch(context, result);
           },
           child: Container(
-            padding: EdgeInsets.symmetric(vertical: 10,horizontal: 10),
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
             alignment: Alignment.centerLeft,
             width: double.infinity,
             decoration: BoxDecoration(
@@ -25,31 +34,33 @@ class SearchBar extends StatelessWidget {
                 borderRadius: BorderRadius.circular(20),
                 boxShadow: [
                   BoxShadow(
-                      color: Colors.black12, blurRadius: 5, offset: Offset(0, 5))
+                      color: Colors.black12,
+                      blurRadius: 5,
+                      offset: Offset(0, 5))
                 ]),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Text('¿A dónde iremos hoy?',style: TextStyle(color: Colors.black87, ),),
-                    Icon(Icons.search)
-                  ],
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(
+                  '¿A dónde iremos hoy?',
+                  style: TextStyle(
+                    color: Colors.black87,
+                  ),
                 ),
+                Icon(Icons.search)
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 
-
-
-  void returnSearch(BuildContext context,SearchResult result){
-    
+  void returnSearch(BuildContext context, SearchResult result) {
     final blocSearch = BlocProvider.of<SearchBloc>(context);
 
-    if(result.cancel) return ;
-    if(result.manual) 
-    blocSearch.add(OnActiveManualMarker());
-    return ;
+    if (result.cancel) return;
+    if (result.manual) blocSearch.add(OnActiveManualMarker());
+    return;
   }
 }
- 
