be7c373c011b0fb09a6f6fd70a8543bf3e3ccb9c
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-02-07 22:20:07 -0500

    Manual Market init widget

--- Projects/maps_app/lib/widgets/search_bar.dart
+++ Projects/maps_app/lib/widgets/search_bar.dart
@@ -48,3 +48,4 @@ class SearchBar extends StatelessWidget {
     if(result.cancel) return ;
   }
 }
+ 
--- Projects/maps_app/lib/widgets/widgets.dart
+++ Projects/maps_app/lib/widgets/widgets.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:maps_app/bloc/Map/map_bloc.dart';
+import 'package:maps_app/models/search_result.dart';
 import 'package:maps_app/search/search_destination.dart';
 
 import 'package:meta/meta.dart';
 
 part 'my_location_button.dart';
+part 'manual_marker.dart';
 part 'my_route_button.dart';
 part 'my_follow_location.dart';
 part 'search_bar.dart';
