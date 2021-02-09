fcf7dff4943abfa1f3659cfa707734c400ad7c9a
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-02-08 20:23:39 -0500

    Include traffic service files

---
+++ Projects/maps_app/lib/services/traffic_service.dart
@@ -0,0 +1,27 @@
+
+
+
+
+import 'package:dio/dio.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class TrafficService {
+  //Singleton 
+
+  TrafficService._privateConstructor();
+
+  static final TrafficService _instance = TrafficService._privateConstructor();
+
+  factory TrafficService(){
+    return _instance;
+  }
+
+  final _dio = Dio();
+
+  Future getCoordsStartAndEnd(LatLng start,LatLng end) async {
+
+      print(start);
+  }
+
+
+}
