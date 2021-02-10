e769f041071900c2a0b0684f52f7268d939bd232
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-02-09 19:43:47 -0500

    AlertDialog Cupertino calculate route

--- Projects/maps_app/lib/helpers/calculate_alert.dart
+++ Projects/maps_app/lib/helpers/calculate_alert.dart
@@ -1,10 +1,16 @@
-
-
 part of 'helpers.dart';
 
-
-void calculateAlert(BuildContext context){
-  if(Platform.isAndroid){
-    showDialog(context: context, builder: (context) => AlertDialog(title: Text('Espera por favor...'),content: Text('Calculando ruta'),),);
-  }
+void calculateAlert(BuildContext context) {
+  if (Platform.isAndroid)
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Espera por favor...'),
+        content: Text('Calculando ruta'),
+      ),
+    );
+  else showCupertinoDialog(context: context, builder: (context) => CupertinoAlertDialog(
+    title: Text('Espere por favor'),
+    content: CupertinoActivityIndicator(),
+  ));
 }
