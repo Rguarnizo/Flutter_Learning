e00b8cca885dcaf48619bef080e65e4adc0d52be
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-02-10 21:50:18 -0500

    Add markers parameter in state

--- Projects/maps_app/lib/bloc/Map/map_state.dart
+++ Projects/maps_app/lib/bloc/Map/map_state.dart
@@ -9,19 +9,29 @@ class MapInitial {
 
   //Polylines
   final Map<String, Polyline> polylines;
+  final Map<String, Marker> markers;
 
   MapInitial({
     Map<String, Polyline> polylines,
-    LatLng centralLocation,    
+    Map<String, Marker> markers,
+    LatLng centralLocation,
     this.mapReady = false,
     this.drawTravel = true,
     this.followLocation = false,
-  }) : this.polylines = polylines ?? new Map(),this.centralLocation = centralLocation?? new LatLng(0,0);
+  })  : this.polylines = polylines ?? new Map(),
+         this.markers = markers ?? new Map(),
+       this.centralLocation = centralLocation ?? new LatLng(0, 0);
 
-  MapInitial copyWith({bool mapReady, bool drawTravel,bool followLocation,Map<String, Polyline> polylines,LatLng centralLocation}) => MapInitial(
-      mapReady: mapReady ?? this.mapReady,
-      drawTravel: drawTravel ?? this.drawTravel,
-      polylines: polylines ?? this.polylines,
-      followLocation: followLocation?? this.followLocation,
-      centralLocation: centralLocation?? this.centralLocation);
+  MapInitial copyWith(
+          {bool mapReady,
+          bool drawTravel,
+          bool followLocation,
+          Map<String, Polyline> polylines,
+          LatLng centralLocation}) =>
+      MapInitial(
+          mapReady: mapReady ?? this.mapReady,
+          drawTravel: drawTravel ?? this.drawTravel,
+          polylines: polylines ?? this.polylines,
+          followLocation: followLocation ?? this.followLocation,
+          centralLocation: centralLocation ?? this.centralLocation);
 }
