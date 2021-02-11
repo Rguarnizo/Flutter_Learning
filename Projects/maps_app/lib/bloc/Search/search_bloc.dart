0f200fd920a1fd58f17c46f9c9465ffcec9d36e4
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-02-10 20:52:26 -0500

    Create historial in SearchState"

--- Projects/maps_app/lib/bloc/Search/search_bloc.dart
+++ Projects/maps_app/lib/bloc/Search/search_bloc.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:maps_app/models/search_result.dart';
 import 'package:meta/meta.dart';
 
 part 'search_event.dart';
--- Projects/maps_app/lib/bloc/Search/search_state.dart
+++ Projects/maps_app/lib/bloc/Search/search_state.dart
@@ -3,9 +3,13 @@ part of 'search_bloc.dart';
 @immutable
 class SearchState {
   final bool seleccionManual;
+  final List<SearchResult> historial;
 
-  SearchState({this.seleccionManual = false});
+  SearchState({this.seleccionManual = false, List<SearchResult> historial})
+      : this.historial = (historial == null) ? []:historial;
 
-  SearchState copyWith({bool seleccionManual}) =>
-      SearchState(seleccionManual: seleccionManual ?? this.seleccionManual);
+  SearchState copyWith({bool seleccionManual, List<SearchResult> historial}) =>
+      SearchState(
+          seleccionManual: seleccionManual ?? this.seleccionManual,
+          historial: historial ?? this.historial);
 }
