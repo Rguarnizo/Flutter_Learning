ffc774832db730ff72554f928a132dff30b52aec
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-02-10 21:24:28 -0500

    Fix bug, duplicate historial results

--- Projects/maps_app/lib/bloc/Search/search_bloc.dart
+++ Projects/maps_app/lib/bloc/Search/search_bloc.dart
@@ -17,8 +17,17 @@ class SearchBloc extends Bloc<SearchEvent, SearchState> {
 
     if(event is OnActiveManualMarker) yield state.copyWith(seleccionManual: true);
     if(event is OnDeactiveManualMarker) yield state.copyWith(seleccionManual: false);
-    if(event is OnAddHistorial) state.historial.add(event._searchResult); yield state.copyWith(historial: state.historial);
+    if(event is OnAddHistorial) yield* this._onAddHistorial(event);
 
 
+  }
+
+  Stream<SearchState> _onAddHistorial(OnAddHistorial event) async*{
+    
+    final exist = state.historial.where((element) => element.nombreDestino == event._searchResult.nombreDestino).length;
+    if(exist == 0){
+      state.historial.add(event._searchResult); 
+      yield state.copyWith(historial: state.historial);
+    }
   }
 }
