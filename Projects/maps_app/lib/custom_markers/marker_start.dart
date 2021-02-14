381004064ce60d9aafe456f891a6ac89fd74fb45
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-02-13 21:25:52 -0500

    Continue a custom marker with CustomPainter

--- Projects/maps_app/lib/custom_markers/marker_start.dart
+++ Projects/maps_app/lib/custom_markers/marker_start.dart
@@ -11,6 +11,21 @@ class MarkerStart extends CustomPainter {
         
     canvas.drawCircle(Offset(radius, size.height-radius),radius, paint);
     canvas.drawCircle(Offset(radius, size.height-radius),radiusWhite, paint..color= Colors.white);
+
+    final Path path = new Path();
+    path.moveTo(40, 20);
+    path.lineTo(size.width-10, 20);
+    path.lineTo(size.width-10, 100);
+    path.lineTo(40, 100);
+
+    canvas.drawShadow(path, Colors.black87,10, false);
+
+    //Caja blanca
+    final whiteBox = Rect.fromLTWH(40, 20, size.width-60, 80);
+    canvas.drawRect(whiteBox, paint);
+    
+    final blackBox = Rect.fromLTWH(40, 20, 70, 80);
+    canvas.drawRect(blackBox, paint..color = Colors.black);
   }
 
   @override
