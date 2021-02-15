caaa82d3a908f4f1aafedd4d5a9ac4c4f001e104
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-02-14 19:29:56 -0500

    Fix bugs and add file

---
+++ Projects/maps_app/lib/helpers/widgets_to_marker.dart
@@ -0,0 +1,21 @@
+part of 'helpers.dart';
+
+
+
+Future<BitmapDescriptor> getMarkerStartIcon(int seg) async {
+
+  final recorder = PictureRecorder();
+  final canvas = Canvas(recorder);
+  final size = new Size(350, 150);
+  
+  final markerStart = new MarkerStart(seg/60.floor());
+
+  markerStart.paint(canvas, size);
+
+  final picture = recorder.endRecording();
+  final image = await picture.toImage(size.width.toInt(), size.height.toInt());
+  final data = await image.toByteData(format: ImageByteFormat.png);
+
+  return BitmapDescriptor.fromBytes(data.buffer.asUint8List());
+
+}
