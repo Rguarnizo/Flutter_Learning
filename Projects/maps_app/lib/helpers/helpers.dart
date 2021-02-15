184122a7589ee7df6ae00dbe2a3096b0a83cfcf7
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-02-14 19:29:28 -0500

    Convert widget to bitmapdescriptor

--- Projects/maps_app/lib/helpers/helpers.dart
+++ Projects/maps_app/lib/helpers/helpers.dart
@@ -5,7 +5,9 @@ import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart' show BitmapDescriptor;
+import 'package:maps_app/custom_markers/custom_markers.dart';
 
 part 'modal_popup.dart';
 part 'calculate_alert.dart';
 part 'custom_image_markers.dart';
+part 'widgets_to_marker.dart';
