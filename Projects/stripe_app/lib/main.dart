234fb21f91d5c5c3eae6982b9d53e908bf9d4fdb
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-02-15 18:51:52 -0500

    Pages and Routes

--- Projects/stripe_app/lib/main.dart
+++ Projects/stripe_app/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:stripe_app/pages/check_pay_page.dart';
+import 'package:stripe_app/pages/home_page.dart';
  
 void main() => runApp(MyApp());
  
@@ -7,15 +9,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
+      routes: {
+        'home' : (_) => HomePage(),
+        'checkPay': (_) => CheckPayPage(),
+      },
+      theme: ThemeData.light().copyWith(
+        primaryColor: Color(0xff284879),
+        scaffoldBackgroundColor: Color(0xff21232A)
       ),
     );
   }
