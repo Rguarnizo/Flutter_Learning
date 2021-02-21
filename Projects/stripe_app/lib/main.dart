3b6e92b57e8c9876ed7742c0688591a2ee25a360
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-02-20 22:49:14 -0500

    Stripe service init, and create new card method payment

--- Projects/stripe_app/lib/main.dart
+++ Projects/stripe_app/lib/main.dart
@@ -3,12 +3,22 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:stripe_app/bloc/Pay/pay_bloc.dart';
 import 'package:stripe_app/pages/check_pay_page.dart';
 import 'package:stripe_app/pages/home_page.dart';
+import 'package:stripe_app/services/stripe_service.dart';
  
 void main() => runApp(MyApp());
  
 class MyApp extends StatelessWidget {
+
+
+  
+
+
   @override
   Widget build(BuildContext context) {
+
+    StripeService().init();
+    
+
     return MultiBlocProvider(
       providers: [
         BlocProvider(create: (context) => PayBloc(),),
--- Projects/stripe_app/lib/services/stripe_service.dart
+++ Projects/stripe_app/lib/services/stripe_service.dart
@@ -1,12 +1,8 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:stripe_app/models/stripe_custom_response.dart';
 import 'package:stripe_payment/stripe_payment.dart';
 
-class StripeService{
-
-
-
+class StripeService {
   StripeService._privateContructor();
 
   static final StripeService _instance = StripeService._privateContructor();
@@ -14,60 +10,53 @@ class StripeService{
   factory StripeService() => _instance;
 
   String _paymentApiUrl = 'https://api.stripe.com/v1/payment_intents';
-  String _secretKey = 'sk_test_51IN8WbFDFJjKdaSPfodYjqjDcNhLla3LYhemNkVVxGYSHQ3bzhJLxKFPFnSwFnZu5Hf5CqCRiifMqA7aAYR4diiw00f8JjXwvh';
+  String _secretKey =
+      'sk_test_51IN8WbFDFJjKdaSPfodYjqjDcNhLla3LYhemNkVVxGYSHQ3bzhJLxKFPFnSwFnZu5Hf5CqCRiifMqA7aAYR4diiw00f8JjXwvh';
+  String _apiKey =
+      'pk_test_51IN8WbFDFJjKdaSPDCcO66I9MkLhSEzSnBv7SBxgjXXZYZZWOb9RUiH9oX0dePUGu95rnmtFvtKcg0Gk8TOaYnw100QAV1WdiI';
 
-  void init(){
-
+  void init() {
+    StripePayment.setOptions(StripeOptions(
+        publishableKey: this._apiKey,
+        androidPayMode: 'test',
+        merchantId: 'test'));
   }
 
   Future payWithExistCard({
     @required String amount,
     @required String currency,
-    @required CreditCard card,    
+    @required CreditCard card,
+  }) async {}
+
+  Future payWithNewCard({
+    @required String amount,
+    @required String currency,
   }) async {
+    try {
+      final paymentMethod =
+          await StripePayment.paymentRequestWithCardForm(CardFormPaymentRequest());
 
-  }
+          //TODO: Crear un intent.
 
-  Future payWithNewCard(
-    {
-      @required String amount,
-      @required String currency,
+      return StripeCustomResponse(ok: true,msg: 'Todo Correcto!');
+    } catch (e) {
+      return StripeCustomResponse(ok: false, msg: e.toString());
     }
-  ) async {
-
   }
 
-  
-  Future payWithAppleOrGoogle(
-    {
-      @required String amount,
-      @required String currency,
-    }
-  ) async {
+  Future payWithAppleOrGoogle({
+    @required String amount,
+    @required String currency,
+  }) async {}
 
-  }
+  Future _createPaymentIntent({
+    @required String amount,
+    @required String currency,
+  }) async {}
 
-  Future _createPaymentIntent(
-    {
-      @required String amount,
-      @required String currency,
-    }
-  ) async {
-
-  }
-
-  Future _realicePay(
-    {
-      @required String amount,
-      @required String currency,
-      @required PaymentMethod paymentMethod,
-
-    }
-  ) async {
-
-  }
-
-
-
-
+  Future _realicePay({
+    @required String amount,
+    @required String currency,
+    @required PaymentMethod paymentMethod,
+  }) async {}
 }
