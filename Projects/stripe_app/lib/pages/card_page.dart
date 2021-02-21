bb4076f8c2f9c5c74beefb8eaebada41fa7ecdbe
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-02-20 21:49:35 -0500

    Hero tag with BlocProvider, show correctly animation

--- Projects/stripe_app/lib/pages/card_page.dart
+++ Projects/stripe_app/lib/pages/card_page.dart
@@ -8,19 +8,7 @@ import 'package:stripe_app/widgets/total_pay_button.dart';
 class CardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-
     final payBloc = BlocProvider.of<PayBloc>(context);
-
-    final card = CreditsCard(
-        cardNumberHidden: '5555',
-        cardNumber: '5555555555554444',
-        brand: 'mastercard',
-        cvv: '213',
-        expiracyDate: '01/25',
-        cardHolderName: 'Melissa Flores');
-
-
 
     return Scaffold(
         appBar: AppBar(
@@ -36,15 +24,22 @@ class CardPage extends StatelessWidget {
         body: Stack(
           children: [
             Container(),
-            Hero(
-              tag: card.cardNumber,
-              child: CreditCardWidget(
-                cardNumber: card.cardNumberHidden,
-                expiryDate: card.expiracyDate,
-                cardHolderName: card.cardHolderName,
-                cvvCode: card.cvv,
-                showBackView: false,
-              ),
+            BlocBuilder<PayBloc, PayState>(
+              builder: (context, state) {
+
+                final card = state.card;
+                
+                return Hero(
+                  tag: card.cardNumber,
+                  child: CreditCardWidget(
+                    cardNumber: card.cardNumberHidden,
+                    expiryDate: card.expiracyDate,
+                    cardHolderName: card.cardHolderName,
+                    cvvCode: card.cvv,
+                    showBackView: false,
+                  ),
+                );
+              },
             ),
             Positioned(
               bottom: 0,
