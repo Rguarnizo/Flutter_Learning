f37e4bf0895de059e7a82324e2f8b464e5b39e02
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-02-21 21:12:21 -0500

    Adding code of package

--- Projects/easy_route_transitions/easy_route_transitions/lib/easy_route_transitions.dart
+++ Projects/easy_route_transitions/easy_route_transitions/lib/easy_route_transitions.dart
@@ -1,7 +1,43 @@
 library easy_route_transitions;
 
 /// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+enum AnimationType { normal, fadeIn }
+
+class RouteTransitions {
+  final BuildContext context;
+  final Widget child;
+  final AnimationType animation;
+  final Duration duration;
+  final bool replacement;
+
+  RouteTransitions(
+      {@required this.context,
+      @required this.child,
+      this.replacement = false,
+      this.duration = const Duration(milliseconds: 300),
+      this.animation = AnimationType.normal}){
+    switch (animation) {
+      case AnimationType.normal:
+        Navigator.push(context, MaterialPageRoute(builder: (_) => this.child));
+        break;
+      case AnimationType.fadeIn:
+        this._fadeInTransition();
+        break;
+    }
+  }
+
+  void _fadeInTransition() {
+    final route = PageRouteBuilder(
+        pageBuilder: (context, animation, secondaryAnimation) => this.child,
+        transitionDuration: duration,
+        transitionsBuilder: (_,animation, __, child){
+          return FadeTransition(child: child,opacity: Tween<double>(begin: 0.0,end: 1.0).animate(CurvedAnimation(parent: animation, curve: Curves.easeOut)));
+        });
+
+    navigateToPage(route);
+  }
+
+  void navigateToPage(Route route) => this.replacement? Navigator.pushReplacement(context, route):Navigator.push(context, route);
 }
