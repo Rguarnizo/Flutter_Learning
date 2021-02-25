46d05c1cf329fe9fed7b090e9927330382f22662
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-02-24 20:20:20 -0500

    Formating code

--- Projects/easy_route_transitions/easy_route_transitions/lib/easy_route_transitions.dart
+++ Projects/easy_route_transitions/easy_route_transitions/lib/easy_route_transitions.dart
@@ -17,7 +17,7 @@ class RouteTransitions {
       @required this.child,
       this.replacement = false,
       this.duration = const Duration(milliseconds: 300),
-      this.animation = AnimationType.normal}){
+      this.animation = AnimationType.normal}) {
     switch (animation) {
       case AnimationType.normal:
         Navigator.push(context, MaterialPageRoute(builder: (_) => this.child));
@@ -32,12 +32,17 @@ class RouteTransitions {
     final route = PageRouteBuilder(
         pageBuilder: (context, animation, secondaryAnimation) => this.child,
         transitionDuration: duration,
-        transitionsBuilder: (_,animation, __, child){
-          return FadeTransition(child: child,opacity: Tween<double>(begin: 0.0,end: 1.0).animate(CurvedAnimation(parent: animation, curve: Curves.easeOut)));
+        transitionsBuilder: (_, animation, __, child) {
+          return FadeTransition(
+              child: child,
+              opacity: Tween<double>(begin: 0.0, end: 1.0).animate(
+                  CurvedAnimation(parent: animation, curve: Curves.easeOut)));
         });
 
     navigateToPage(route);
   }
 
-  void navigateToPage(Route route) => this.replacement? Navigator.pushReplacement(context, route):Navigator.push(context, route);
+  void navigateToPage(Route route) => this.replacement
+      ? Navigator.pushReplacement(context, route)
+      : Navigator.push(context, route);
 }
