5910d6f2f7eb8bd73e2f001553e5b75aa67b6936
Rubén Darío Guarnizo Martínez <43024059+Rguarnizo@users.noreply.github.com>
2021-02-25 21:00:56 -0500

    UI App

--- Projects/auth_google_apple/lib/main.dart
+++ Projects/auth_google_apple/lib/main.dart
@@ -1,22 +1,47 @@
 import 'package:flutter/material.dart';
- 
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'AuthApp',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('AuthApp-Google-Apple'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
+          appBar: AppBar(
+            title: Text('AuthApp-Google-Apple'),
+            actions: [
+              IconButton(
+                icon: Icon(
+                  FontAwesomeIcons.doorOpen,
+                ),
+                onPressed: () {},
+              ),
+            ],
           ),
-        ),
-      ),
+          body: Container(
+            child: Center(
+              child: Column(
+                children: [
+                  MaterialButton(
+                    onPressed: () {},
+                    splashColor: Colors.transparent,
+                    minWidth: double.infinity,
+                    height: 40,
+                    color: Colors.red,
+                    shape: RoundedRectangleBorder(),
+                    child: Row(
+                      children: [
+                        Icon(FontAwesomeIcons.google,color: Colors.white,),
+                        Text('SingIn With Google',style: TextStyle(color: Colors.white),)
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )),
     );
   }
 }
